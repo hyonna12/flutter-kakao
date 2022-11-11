63eb3f351af3a5ec55bb52c4ae23b7e77dc812cb
hyonna12 <hyonna01@gmail.com>
2022-11-11 14:05:57 +0900

    chat_room_screen - chatCard 탭 이벤트 넣기

--- lib/components/chat_card.dart
+++ lib/components/chat_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kakaotalk/models/chat.dart';
+import 'package:flutter_kakaotalk/screens/chat_room_screen.dart';
 
 class ChatCard extends StatelessWidget {
   const ChatCard({Key? key, required this.chat}) : super(key: key);
@@ -8,49 +9,62 @@ class ChatCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 25,
-              backgroundImage: NetworkImage(chat.image),
-            ),
-            title: Text(
-              chat.title,
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 16,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatRoomScreen(),
+          ),
+        );
+      },
+      child: Container(
+        child: Row(
+          children: [
+            Expanded(
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(chat.image),
+                ),
+                title: Text(
+                  chat.title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+                subtitle: Text(chat.content),
+                contentPadding: const EdgeInsets.all(0),
               ),
             ),
-            subtitle: Text(chat.content),
-            contentPadding: const EdgeInsets.all(0),
-          ),
-        ),
-        Column(
-          children: [
-            Text(
-              chat.time,
-              style: TextStyle(color: Color(0xFFa5a5a5), fontSize: 12),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            // 읽지 않은 메시지가 존재할 때 메시지 개수를 알려줌
-            if (chat.count != "0")
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xFFde6344)),
-                child: Text(
-                  chat.count,
-                  style: TextStyle(color: Colors.white, fontSize: 12),
+            Column(
+              children: [
+                Text(
+                  chat.time,
+                  style: TextStyle(color: Color(0xFFa5a5a5), fontSize: 12),
                 ),
-              )
+                SizedBox(
+                  height: 5,
+                ),
+                // 읽지 않은 메시지가 존재할 때 메시지 개수를 알려줌
+                if (chat.count != "0")
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xFFde6344)),
+                    child: Text(
+                      chat.count,
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  )
+              ],
+            )
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 }

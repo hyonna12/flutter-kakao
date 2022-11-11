221e6c898d3609878496020c614ae44725689dbd
hyonna12 <hyonna01@gmail.com>
2022-11-11 09:58:52 +0900

    main_screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,11 +9,47 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectIndex = 0;
+// bottomNavigationBar 아이콘을 선택하면 현재 인덱스에 해당하는 위젯이 화면에 나타남
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MainScreen"),
+      body: IndexedStack(
+        index: _selectIndex,
+        children: [
+          Center(child: Text("IndexedStack 1")),
+          Center(child: Text("IndexedStack 2")),
+          Center(child: Text("IndexedStack 3")),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectIndex,
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index) {
+          // 선택된 아이콘의 index 번호를 알려주고 이 값을 _selectIndex에 저장하여
+          // IndexStack에게 어느 화면이 보여져야 하는지 알려줌
+          setState(() {
+            _selectIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.user),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comment),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.ellipsisH),
+            label: "",
+          ),
+        ],
       ),
     );
   }

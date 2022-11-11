1498491de17140cf024e2983656300c3e0b5ccec
hyonna12 <hyonna01@gmail.com>
2022-11-11 11:27:08 +0900

    profile_screen - 버튼위젯, 앱바

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakaotalk/components/round_icon_button.dart';
 import 'package:flutter_kakaotalk/models/user.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key, required this.user}) : super(key: key);
@@ -18,8 +20,24 @@ class ProfileScreen extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: Text("ProfileScreen"),
+        body: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            icon: Icon(
+              FontAwesomeIcons.times,
+              size: 30,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            RoundIconButton(icon: FontAwesomeIcons.gift),
+            SizedBox(width: 15),
+            RoundIconButton(icon: FontAwesomeIcons.cog),
+            SizedBox(width: 20),
+          ],
         ),
       ),
     );

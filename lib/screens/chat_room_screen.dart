b4e5cbc2188f1b51d4591a1936675103726a1c15
hyonna12 <hyonna01@gmail.com>
2022-11-11 14:19:53 +0900

    chat_room_screen - timeline 위젯

--- lib/screens/chat_room_screen.dart
+++ lib/screens/chat_room_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakaotalk/components/time_line.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ChatRoomScreen extends StatefulWidget {
@@ -9,22 +10,36 @@ class ChatRoomScreen extends StatefulWidget {
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFFb2c7da),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFb2c7da),
+        child: Scaffold(
           backgroundColor: Colors.transparent,
-          title: Text(
-            "홍길동",
-            style: Theme.of(context).textTheme.headline6,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            title: Text(
+              "홍길동",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            actions: [
+              Icon(FontAwesomeIcons.search, size: 20),
+              SizedBox(width: 25),
+              Icon(FontAwesomeIcons.bars, size: 20),
+              SizedBox(width: 25),
+            ],
           ),
-          actions: [
-            Icon(FontAwesomeIcons.search, size: 20),
-            SizedBox(width: 25),
-            Icon(FontAwesomeIcons.bars, size: 20),
-            SizedBox(width: 25),
-          ],
+          body: Column(
+            children: [
+              Expanded(
+                  child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TimeLine(time: "2021년 1월 1일 금요일"),
+                  ],
+                ),
+              ))
+            ],
+          ),
         ),
       ),
     );
